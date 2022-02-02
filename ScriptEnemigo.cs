0efc197f59f5d7cc4c5849f8d281b93e6ef92974
DESKTOP-LALVV45\chema <joloca02@floridauniversitaria.es>
2022-02-02 16:35:21 +0100

    ejercicio 4

--- ScriptEnemigo.cs
+++ ScriptEnemigo.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class ScriptEnemigo : MonoBehaviour
 {
+    float rate = 1f;
     bool limite = false;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Movimiento", 1f, 1f);
+        InvokeRepeating("Movimiento", 1f, rate);
     }
 
     // Update is called once per frame
@@ -16,14 +17,34 @@ public class ScriptEnemigo : MonoBehaviour
     {
         
     }
+  
+    IEnumerator CambioAltura()
+    {
+        yield return new WaitForSeconds(rate);
+        CancelInvoke();
+        transform.Translate(Vector3.down);
+        if(transform.position.x == 5)
+        {
+            transform.Translate(Vector3.right);
+        }
+        else if(transform.position.x == -5)
+        {
+            transform.Translate(Vector3.left);
+        }
+        rate *= 0.9f;
+        InvokeRepeating("Movimiento", rate, rate);
+    }
     void Movimiento()
     {
         if (limite == false)
         {
+            
             transform.Translate(Vector3.right);
             if (transform.position.x == 6)
             {
+                StartCoroutine(CambioAltura());
                 limite = true;
+
             }
         }
         else
@@ -31,6 +52,7 @@ public class ScriptEnemigo : MonoBehaviour
             transform.Translate(Vector3.left);
             if (transform.position.x == -6)
             {
+                StartCoroutine(CambioAltura());
                 limite = false;
             }
         }
